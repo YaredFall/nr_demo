bcec045e3ea9d8ec01d065fe18af3df40adf317f
YaredFall <yaredfall@gmail.com>
2022-05-12 15:06:13 +0700

    Убрал лишнее из основной ветки

--- НаучнаяРабота_пробная_версия/Program.cs
+++ НаучнаяРабота_пробная_версия/Program.cs
@@ -11,81 +11,6 @@ namespace НаучнаяРабота_пробная_версия
     
     class Program
     {
-        static void TestCase()
-        {
-            /*            Material ironOre = new Material("iron ore", 200);
-                Material copperOre = new Material("copper ore", 300);
-
-                Product ironIngot = new Product("iron ingot", 1000, new List<ProductPart> { new ProductPart( ironOre, 10) });
-                Product copperIngot = new Product("copper ingot", 1000, new List<ProductPart> { new ProductPart(copperOre, 10) });
-                Product wire = new Product("wire", 10000, new List<ProductPart>() { new ProductPart(ironIngot, 2), new ProductPart(copperIngot, 1) });
-
-                Console.WriteLine(ironOre);
-                Console.WriteLine(copperOre);
-                Console.WriteLine(ironIngot);
-                Console.WriteLine(copperIngot);
-                Console.WriteLine(wire);
-
-                InitialComponents avaiable = new InitialComponents(new List<ProductPart> { new ProductPart(ironOre, 10000), new ProductPart(copperOre, 5000), new ProductPart(wire, 100) });
-                Console.WriteLine("availavle: " + avaiable);
-
-                JsonSerializerOptions jsonOptions = new JsonSerializerOptions { WriteIndented = true };
-                string jsonString = JsonSerializer.Serialize(avaiable, jsonOptions);
-                Console.WriteLine(jsonString);
-
-                InitialComponents deserialized =
-                    JsonSerializer.Deserialize<InitialComponents>(jsonString);
-                Console.WriteLine("deserialized: " + deserialized);
-
-                jsonString = JsonSerializer.Serialize(deserialized, jsonOptions);
-                Console.WriteLine(jsonString);*/
-
-            Product wood = new Product("wood", 100, null);
-            Product ironOre = new Product("iron ore", 200, null);
-            Product copperOre = new Product("copper ore", 300, null);
-
-            Product ironIngot = new Product("iron ingot", 1000, new ProductDict { { ironOre, 10 } });
-            Product copperIngot = new Product("copper ingot", 1000, new ProductDict { { copperOre, 10 } });
-            Product wire = new Product("wire", 10000, new ProductDict
-                { {ironIngot, 2 }, {copperIngot, 1 } });
-            Product ironShovel = new Product("iron showel", 20000, new ProductDict
-                { {ironIngot, 10 }, { wood, 100 } });
-
-            List<Product> availavleProductTypes = new List<Product>
-                { wood, ironOre, copperOre, ironIngot, copperIngot, wire, ironShovel };
-
-            Console.WriteLine("Availavle product types:\n " + string.Join("\n ", availavleProductTypes));
-
-            Product weirdProduct = new Product("weird product", 404, new ProductDict
-            {
-                { wood, 5 },
-                { ironIngot, 10 },
-                { wire, 5 },
-                { ironShovel, 2 }
-            });
-
-            Product plank = new Product("plank", 200, new ProductDict { { wood, 10 } });
-            Product stick = new Product("stick", 500, new ProductDict { { plank, 2 } });
-
-            ProductDict initialList = new ProductDict {
-                { ironOre, 10000 },
-                { copperOre, 5000 },
-                { wood, 1000 },
-                { plank, 20 },
-                { stick, 10 }
-            };
-
-            Console.WriteLine("Initial list of products:\n" + initialList);
-
-            ProductManufacturer manufacturer = new ProductManufacturer(initialList);
-
-            Console.WriteLine(manufacturer.MaxCanBeManufactured(stick));
-
-
-            /*            manufacturer.Manufacture(wire, 500);
-                        Console.WriteLine("Current list of products:\n" + initialList);*/
-        }
-
 
         static void DemoCase()
         {
@@ -101,13 +26,6 @@ namespace НаучнаяРабота_пробная_версия
                 { ironIngot, 3 },
                 { ironRod, 1 }
             };
-
-            ProductManufacturer manufacturer = new ProductManufacturer(availableProducts);
-
-            //Console.WriteLine(manufacturer.MaxCanBeManufactured(ironOre));
-            //Console.WriteLine(manufacturer.MaxCanBeManufactured(ironIngot));
-            //Console.WriteLine(manufacturer.MaxCanBeManufactured(ironRod));
-            //Console.WriteLine(manufacturer.MaxCanBeManufactured(hammer));
 
             UseWebsiteToSolve(existingProducts, availableProducts);
 
